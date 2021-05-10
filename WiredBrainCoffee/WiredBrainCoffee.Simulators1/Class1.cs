d4869bdb317ec0bd516cbeb424728f7e720fc8f5
Massiel <massiel.dev.ca@gmail.com>
2021-05-09 23:43:54 -0400

    .

---
+++ WiredBrainCoffee/WiredBrainCoffee.Simulators1/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WiredBrainCoffee.Simulators1
+{
+    public class Class1
+    {
+    }
+}
---
+++ WiredBrainCoffee/WiredBrainCoffee.UI.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using WiredBrainCoffee.Simulators;
+
+namespace WiredBrainCoffee.UI.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CoffeeMachine coffeeMachine;
+        public MainWindow()
+        {
+            InitializeComponent();
+            coffeeMachine = new CoffeeMachine();
+            txtCappuccinoCounter.Text = coffeeMachine.CounterCappuccino.ToString();
+
+        }
+
+        private void ButtonMakeCappuccinoClick(object sender, RoutedEventArgs e)
+        {
+            coffeeMachine.MakeCappuccino();
+            txtCappuccinoCounter.Text = coffeeMachine.CounterCappuccino.ToString();
+        }
+    }
+}
