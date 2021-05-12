a88c61e06f67e8dc64a885c6997a2ebbf3d2bafe
Massiel <massiel.dev.ca@gmail.com>
2021-05-11 23:19:21 -0400

    added state

--- WiredBrainCoffee/WiredBrainCoffee.Simulators/CoffeeMachine.cs
+++ WiredBrainCoffee/WiredBrainCoffee.Simulators/CoffeeMachine.cs
@@ -1,12 +1,23 @@
-﻿namespace WiredBrainCoffee.Simulators
+﻿using WiredBrainCoffee.Storage;
+
+namespace WiredBrainCoffee.Simulators
 {
     public class CoffeeMachine
     {
+        private CoffeeMachineStateSaver _coffeeMachineStateSaver;
         public int CounterCappuccino { get; private set; }
+
+        public CoffeeMachine()
+        {
+            _coffeeMachineStateSaver = new CoffeeMachineStateSaver();
+            var state = _coffeeMachineStateSaver.Load();
+            CounterCappuccino = state.CounterCappuccino;
+        }                
 
         public void MakeCappuccino()
         {
             CounterCappuccino++;
+            _coffeeMachineStateSaver.Save(new CoffeeMachineState { CounterCappuccino = CounterCappuccino});
         }
     }
 }
---
+++ WiredBrainCoffee/WiredBrainCoffee.Storage/CoffeeMachineStateSaver.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace WiredBrainCoffee.Storage
+{
+    public class CoffeeMachineStateSaver
+    {
+        private string _fileName = "machine.json";
+
+        public void Save(CoffeeMachineState state)
+        {
+            var json = JsonConvert.SerializeObject(state);
+            File.WriteAllText(_fileName, json);
+        }
+
+        public CoffeeMachineState Load()
+        {
+            if (File.Exists(_fileName))
+            {
+                var json = File.ReadAllText(_fileName);
+                return JsonConvert.DeserializeObject<CoffeeMachineState>(json);
+            }
+
+            return new CoffeeMachineState();
+        }
+    }
+}
