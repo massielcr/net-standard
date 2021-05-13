aa005892886719374af080fa0a89b64af25c368e
Massiel <massiel.dev.ca@gmail.com>
2021-05-12 20:26:26 -0400

    clean up

--- WiredBrainCoffee/WiredBrainCoffee.Simulators/CoffeeMachine.cs
+++ WiredBrainCoffee/WiredBrainCoffee.Simulators/CoffeeMachine.cs
@@ -1,22 +1,24 @@
-﻿using WiredBrainCoffee.Storage;
+﻿using System;
+using WiredBrainCoffee.Storage;
 
 namespace WiredBrainCoffee.Simulators
 {
     public class CoffeeMachine
     {
-        private CoffeeMachineStateSaver _coffeeMachineStateSaver;
         public int CounterCappuccino { get; private set; }
+
+        private CoffeeMachineStateSaver _coffeeMachineStateSaver;        
 
         public CoffeeMachine()
         {
             _coffeeMachineStateSaver = new CoffeeMachineStateSaver();
-            var state = _coffeeMachineStateSaver.Load();
-            CounterCappuccino = state.CounterCappuccino;
-        }                
+            CounterCappuccino = _coffeeMachineStateSaver.Load().CounterCappuccino;
+        }
 
         public void MakeCappuccino()
         {
             CounterCappuccino++;
+            Console.WriteLine($"Counter Cappuccino: {CounterCappuccino}");
             _coffeeMachineStateSaver.Save(new CoffeeMachineState { CounterCappuccino = CounterCappuccino});
         }
     }
